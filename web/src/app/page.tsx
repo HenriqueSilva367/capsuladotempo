fcefae8ae892b16fb4537deab3cf12af1c717f53
HenriqueSilva367 <jhenrique367@gmail.com>
2023-05-19 00:56:22 -0300

    Criando Setup da aplicação

---
+++ web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+   <h1>
+    Hello World
+    <Button title="Henrique" />
+    <Button title="Rebeca" />
+    <Button title="Pedro" />
+   </h1>
+
+   
+  )
+}
